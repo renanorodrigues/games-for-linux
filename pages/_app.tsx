fe49b4530c7bb0d0289b4f53bab8673ae648fbcd
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2021-10-22 09:01:36 -0300

    _app.tsx implemented react-toastify package for notifications

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,12 @@ import Head from 'next/head';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from '../store';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css'
+
+toast.configure()
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => { 
   return (
