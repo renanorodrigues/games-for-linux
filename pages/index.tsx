1fd3443126fdf77fee5a550abf29e47b828912d5
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2021-10-22 09:00:54 -0300

    Index page implemented StoreFront component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,44 @@
-import React from 'react';
 import MainComponent from '../components/shared/MainComponent';
-import { useRouter } from 'next/router';
+import StoreFront from '../components/StoreFront';
+import { Carousel } from 'react-bootstrap';
+import styles from './styles.module.css';
 
-const Home: React.FC = () => {
-  const router = useRouter();
-
+const StoreIndex: React.FC = () => {
   return (
     <MainComponent>
-      <button onClick={ () => router.push('/Auth/Login') }>Login</button>
+      <Carousel className={styles.carousel}>
+        <Carousel.Item>
+          <img 
+            className="d-block w-100"
+            src="https://i2.wp.com/itsfoss.com/wp-content/uploads/2018/07/war-thunder.jpg?w=797&ssl=1" 
+            alt="First Slide"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img 
+            className="d-block w-100"
+            src="https://i0.wp.com/itsfoss.com/wp-content/uploads/2018/07/wafku.jpg?w=795&ssl=1" 
+            alt="Second Slide"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img 
+            className="d-block w-100"
+            src="https://i1.wp.com/itsfoss.com/wp-content/uploads/2018/07/star-conflict.jpg?w=782&ssl=1" 
+            alt="Third Slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+
+      <StoreFront title="Offers of the week" btn_style="highlighted"/>
+
+      <StoreFront title="Releases"/>
+
+      <StoreFront title="Most visited"/>
     </MainComponent>
   )
 }
 
-export default Home;
+export default StoreIndex;
